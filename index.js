e098ae9c9a8ebe660df7924714760a1b6bcc40ff
clinton moshe mwachia <clintonmwachia9@gmail.com>
2022-04-19 10:57:16 +0300

    installed required packages

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const cors = require("cors");
+require("dotenv").config();
+
+/** INITIALIZE THE APP */
+const app = express();
+
+/** MIDDLEWARES */
+app.use(helmet());
+app.use(cors());
+app.options("*", cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+/** CONNECTING TO THE DATABSE */
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected successfully"))
+  .catch((err) => console.log("could not connect to database"));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on: http://localhost:${process.env.PORT}/api/v1`);
+});
